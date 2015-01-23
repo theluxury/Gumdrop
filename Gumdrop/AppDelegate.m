95371da32ca7e2577e597dcf27846e3b050d9a60
Mark Wang <zhengkaw@gmail.com>
2015-01-23 14:01:17 -0800

    put both requests into one alert chain

--- Gumdrop/AppDelegate.m
+++ Gumdrop/AppDelegate.m
@@ -41,6 +41,8 @@ NSString * const AUTH_TOKEN = @"AUTH_TOKEN";
 
 - (IBAction)connectToTrello:(id)sender {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults removeObjectForKey:APP_KEY];
+    [defaults removeObjectForKey:AUTH_TOKEN];
 
     // First, make sure there is an appKey
     if (![defaults objectForKey:APP_KEY]) {
@@ -59,30 +61,32 @@ NSString * const AUTH_TOKEN = @"AUTH_TOKEN";
         NSModalResponse response = [alert runModal];
         if(response == NSAlertFirstButtonReturn){
             [defaults setObject:[textField stringValue] forKey:APP_KEY];
+            
+            if (![defaults objectForKey:AUTH_TOKEN]) {
+                // Open a browser window that sends them to https://trello.com/1/authorize?key=substitutewithyourapplicationkey&name=Gumdrop&expiration=1day&response_type=token&scope=read,write, then open an alert that tells them to copy and paste the appkey.
+                NSString *authTokenUrl = [NSString stringWithFormat:@"https://trello.com/1/authorize?key=%@&name=Gumdrop&expiration=1day&response_type=token&scope=read,write", [defaults objectForKey:APP_KEY]];
+                [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:authTokenUrl]];
+                
+                NSAlert *alert = [[NSAlert alloc] init];
+                alert.messageText = @"Please allow Gumdrop to have access to read and write from your Trello boards, then copy and paste the token into this prompt after you allow access. If it did not take you to the right page, please press cancel and try again, making sure your app key is correct.";
+                [alert addButtonWithTitle:@"Ok"];
+                [alert addButtonWithTitle:@"Cancel"];
+                
+                NSTextField *textField = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 400, 24)];
+                [textField selectText:self];
+                [alert setAccessoryView:textField];
+                
+                NSModalResponse response = [alert runModal];
+                if(response == NSAlertFirstButtonReturn)
+                    [defaults setObject:[textField stringValue] forKey:AUTH_TOKEN];
+                else
+                    // If they press cancel, I just assume they pasted in the wrong APP KEY, so get rid of it to restart chain
+                    [defaults removeObjectForKey:APP_KEY];
+            }
         }
-        
         return;
     }
     
-    if (![defaults objectForKey:AUTH_TOKEN]) {
-        // Open a browser window that sends them to https://trello.com/1/authorize?key=substitutewithyourapplicationkey&name=Gumdrop&expiration=1day&response_type=token&scope=read,write, then open an alert that tells them to copy and paste the appkey.
-        NSString *authTokenUrl = [NSString stringWithFormat:@"https://trello.com/1/authorize?key=%@&name=Gumdrop&expiration=1day&response_type=token&scope=read,write", [defaults objectForKey:APP_KEY]];
-        [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:authTokenUrl]];
-        
-        NSAlert *alert = [[NSAlert alloc] init];
-        alert.messageText = @"Please allow Gumdrop to have access to read and write from your Trello boards. Please copy and paste the token into this prompt after you allow access.";
-        [alert addButtonWithTitle:@"Ok"];
-        [alert addButtonWithTitle:@"Cancel"];
-        
-        NSTextField *textField = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 400, 24)];
-        [textField selectText:self];
-        [alert setAccessoryView:textField];
-        
-        NSModalResponse response = [alert runModal];
-        if(response == NSAlertFirstButtonReturn)
-            [defaults setObject:[textField stringValue] forKey:AUTH_TOKEN];
-        return;
-    }
     
     NSString *boardRequestString = [NSString stringWithFormat:@"https://api.trello.com/1/members/me/boards?key=%@&token=%@", [defaults objectForKey:APP_KEY], [defaults objectForKey:AUTH_TOKEN]];
     NSMutableURLRequest *boardRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:boardRequestString]  cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:30.0];
